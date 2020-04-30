763bbc5d8d660bbf69002b8df22c2f7e1b0d77ae
Hyeonseung Yu <hyeonseung.yu@gmail.com>
2020-04-30 16:41:09 -0700

    Camera preview is working.

---
+++ app/src/main/java/com/hyeonseung/arucodetection/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hyeonseung.arucodetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CameraController.newInstance())
+            .commit()
+    }
+}
